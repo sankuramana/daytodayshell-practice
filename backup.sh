226febaa47e6e9cfdc4163405706e54710743a93
ramana sanku <sankuramana456@gmail.com>
2025-10-21 12:18:56 +0530

    this is roboshop

--- backup.sh
+++ backup.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-source_dir=$1
-dest_dir=$2
+days=${3:-14} #if not provided considered as 14 days else it takes thatyou given
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+SCRIPT_NAME=$(basename "$0" .sh)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+mkdir -p "$LOGS_FOLDER"
+echo "Script started execution at: $(date)" | tee -a "$LOG_FILE"
 
-if [ $USERID -ne 0 ]; then 
-echo "ERROR:: please run this script with root priviliage"
-exit 1
-fi
-
-usage(){ 
-echo  -e " $R USAGE:: sudo sh backup.sh <s.dir> <d.dir> <days>[optional] default 14 days $N"
-exit 1
+usage() { 
+  echo -e "${R}USAGE:: sudo bash backup.sh <source_dir> <destination_dir> [days - optional, default 14 days]${N}"
+  exit 1
 }
 
+if [ $USERID -ne 0 ]; then 
+  echo -e "${R}ERROR:: Please run this script with root privilege.${N}"
+  exit 1
+fi
+
 if [ $# -lt 2 ]; then
-usage
+  usage
 fi
 
-if [ ! -d  $source_dir ]; then
-echo -e " $R $source_dir dose not exist $N"
-exit 1
+source_dir=$1
+dest_dir=$2
+
+if [ ! -d "$source_dir" ]; then
+  echo -e "${R}ERROR:: Source directory '$source_dir' does not exist.${N}"
+  exit 1
 fi
 
-if [ ! -d  $dest_dir ]; then
-echo -e " $R $dest_dir dose not exist $N"
-exit 1
+if [ ! -d "$dest_dir" ]; then
+  echo -e "${R}ERROR:: Destination directory '$dest_dir' does not exist.${N}"
+  exit 1
 fi
-
+files_to_find=$(find $source_dir -name "*.log" type f -mtime +4)
+if [ ! -z $files_to_fine  ]; then 
+echo "files found"
+else 
+echo "no files to archive $Y skipping $N"
+fi
