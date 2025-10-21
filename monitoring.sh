ec54b4fef6334c20f7e660fbcfdc5e920300ca36
ramana sanku <sankuramana456@gmail.com>
2025-10-21 19:40:09 +0530

    this is roboshop

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 disk_space=$(df -hT | grep -v Filesystem ) #filtering disk space except filesystem line
+disk_treshold=2
 while IFS= read -r line
 do
 usage=$(echo $line | awk '{print $6}' |cut -d "%" -f1)
 partion=$( echo $line |awk '{ print $7}')
-if [ $usage -ge $disk_space]; then 
+if [ $usage -ge $disk_treshold]; then 
 echo "high usgae on $partion : $usage "
 fi 
 
