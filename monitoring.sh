05a7422d3897160c3525e70888b097f363fbadd6
ramana sanku <sankuramana456@gmail.com>
2025-10-21 19:38:24 +0530

    this is roboshop

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ while IFS= read -r line
 do
 usage=$(echo $line | awk '{print $6}' |cut -d "%" -f1)
 partion=$( echo $line |awk '{ print $7}')
-if [ $susage -ge $disk_space]; then 
+if [ $usage -ge $disk_space]; then 
 echo "high usgae on $partion : $usage "
 fi 
 
