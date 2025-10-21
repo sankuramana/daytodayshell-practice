af2080bb7fd35d0af36475546fba445654e3655d
ramana sanku <sankuramana456@gmail.com>
2025-10-21 19:36:57 +0530

    this is roboshop

--- monitoring.sh
+++ monitoring.sh
@@ -3,6 +3,9 @@ disk_space=$(df -hT | grep -v Filesystem ) #filtering disk space except filesyst
 while IFS= read -r line
 do
 usage=$(echo $line | awk '{print $6}' |cut -d "%" -f1)
-echo "$susage"
+partion=$( echo $line |awk '{ print $7}')
+if [ $susage -ge $disk_space]; then 
+echo "high usgae on $partion : $usage "
+fi 
 
 done <<<$disk_space
