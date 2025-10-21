3cde719eab0392816bdc9d43585f0f7c48312279
ramana sanku <sankuramana456@gmail.com>
2025-10-21 19:30:08 +0530

    this is roboshop

--- monitoring.sh
+++ monitoring.sh
@@ -2,6 +2,6 @@
 disk_space=$(df -hT | grep -v Filesystem ) #filtering disk space except filesystem line
 while IFS= read -r line
 do
-echo " line: $line | cut -d "%" -f1 "
+usage=(echo $line | awk '{print $6}' |cut -d "%" -f1)
 
 done <<<$disk_space
