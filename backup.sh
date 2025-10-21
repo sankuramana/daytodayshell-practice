af65a2892f6486b59223c43079fa51075ea10740
ramana sanku <sankuramana456@gmail.com>
2025-10-21 12:02:32 +0530

    this is roboshop

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 source_dir=$1
-desti_dir=$2
+dest_dir=$2
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
@@ -26,12 +26,14 @@ exit 1
 if [ $# -lt 2 ]; then
 usage
 fi
+
 if [ ! -d  $source_dir ]; then
-echo -e "  $R $source_dir dose not exist $N"
+echo -e " $R $source_dir dose not exist $N"
 exit 1
 fi
-if [ ! -d  $desti_dir ]; then
-echo -e "  $R $desti_dir dose not exist $N"
+
+if [ ! -d  $dest_dir ]; then
+echo -e " $R $dest_dir dose not exist $N"
 exit 1
 fi
 
