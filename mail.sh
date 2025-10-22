06dd723a2f3830e753a50fd89eac1dccb0fa9206
ramana sanku <sankuramana456@gmail.com>
2025-10-22 10:25:33 +0530

    this is roboshop

--- mail.sh
+++ mail.sh
@@ -2,18 +2,27 @@
 
 TO_ADDRESS=$1
 SUBJECT=$2
-Alert_type=$3
-message_body=$4
-ip=$5
+ALERT_TYPE=$3
+MESSAGE_BODY=$4
+IP_ADDRESS=$5
+TO_TEAM=$6
 
+# Read the template file
+TEMPLATE=$(cat template.html)
 
+# Replace placeholders with real data
+FINAL_BODY=$(echo "$TEMPLATE" | sed \
+  -e "s/TO_TEAM/$TO_TEAM/g" \
+  -e "s/ALERT_TYPE/$ALERT_TYPE/g" \
+  -e "s/IP_ADDRESS/$IP_ADDRESS/g" \
+  -e "s|MESSAGE|$MESSAGE_BODY|g")
 
-
-
+# Send the email
 {
-echo "To: $TO_ADDRESS"
-echo "Subject: $SUBJECT"
-echo "Content-Type: text/html"
-echo ""
-echo "$FINAL_BODY"
+  echo "To: $TO_ADDRESS"
+  echo "Subject: $SUBJECT"
+  echo "Content-Type: text/html"
+  echo ""
+  echo "$FINAL_BODY"
 } | msmtp "$TO_ADDRESS"
+                                                                                                                                                                      
